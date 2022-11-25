ab9fcea5619ecbdc5968eb66db289911c3dea563
Brooke Claroni <brookeclaroni@gwu.edu>
2022-11-24 21:53:10 -0500

    add styling

--- script.js
+++ script.js
@@ -10,33 +10,33 @@ function validateForm() {
     var alpha = /^[A-Za-z]+$/;
     if(!firstName.match(alpha))
     {
-        errors += "Non-alphabetic letters were used in first name. ";
+        errors += "<li>Non-alphabetic letters were used in first name.</li>";
         validated = false;
     }
     if(!lastName.match(alpha))
     {
-        errors += "Non-alphabetic letters were used in last name. ";
+        errors += "<li>Non-alphabetic letters were used in last name.</li>";
         validated = false;
     }
 
     // validate firstName and lastName contain more than two characters
     if (firstName.length <= 2) {
-        errors +=  "First name has too few characters. ";
+        errors +=  "<li>First name has too few characters.</li>";
         validated = false;
     }
     if (lastName.length <= 2) {
-        errors += "Last name has too few characters. ";
+        errors += "<li>Last name has too few characters.</li>";
         validated = false;
     }
 
     // validate facilitator name is on list
     if (!(facilitator == "Christian" || facilitator == "Fazil" || facilitator == "Josh" || facilitator == "Chris" || facilitator == "Behdad")) {
-        errors += "Facilitator name not recognized as Christian, Fazil, Josh, Chris, or Behdad. ";
+        errors += "<li>Facilitator name not recognized as Christian, Fazil, Josh, Chris, or Behdad.</li>";
         validated = false;
     }
 
     if (validated == false) {
-        document.getElementById("errorMessage").innerHTML = "Your submission failed due to the following: " + errors;
+        document.getElementById("errorMessage").innerHTML = "Your submission failed due to the following: <ul>" + errors +"</ul>";
         return false;
     }
 
